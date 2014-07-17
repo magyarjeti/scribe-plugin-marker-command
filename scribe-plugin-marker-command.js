f075bf596629d8e83ae6e2fffb3ad569aec03c19
Ács Dániel <acsi@444.hu>
2014-07-17 13:38:45 +0200

    Initial publish

---
+++ scribe-plugin-marker-command.js
@@ -0,0 +1,34 @@
+define("scribe-plugin-marker-command",
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+    __exports__["default"] = function() {
+        return function(scribe) {
+            var markerCommand = new scribe.api.Command('formatBlock');
+            markerCommand.nodeName = 'MARK';
+
+            markerCommand.execute = function() {
+                var selection = new scribe.api.Selection();
+                var range = selection.range;
+
+                    selection.placeMarkers();
+                if (!this.queryState()) {
+                    var wrapper = document.createElement("mark");
+                    range.surroundContents(wrapper);
+                } else {
+                    console.log(range);
+                }
+            };
+
+            markerCommand.queryState = function() {
+                var selection = new scribe.api.Selection();
+                var range = selection.range;
+
+                return range.commonAncestorContainer.nodeName === this.nodeName
+                    || range.commonAncestorContainer.parentNode.nodeName === this.nodeName;
+            };
+
+            scribe.commands.marker = markerCommand;
+        };
+    }
+  });
