7ebb9c17fcc3e81dc8c3fd24dbf863cf436bd155
Ács Dániel <acsi@444.hu>
2014-08-28 17:23:01 +0200

    Update distribution files

--- scribe-plugin-marker-command.js
+++ scribe-plugin-marker-command.js
@@ -1,34 +1,41 @@
-define("scribe-plugin-marker-command",
-  ["exports"],
-  function(__exports__) {
-    "use strict";
-    __exports__["default"] = function() {
-        return function(scribe) {
-            var markerCommand = new scribe.api.Command('formatBlock');
-            markerCommand.nodeName = 'MARK';
-
-            markerCommand.execute = function() {
-                var selection = new scribe.api.Selection();
-                var range = selection.range;
-
-                    selection.placeMarkers();
-                if (!this.queryState()) {
-                    var wrapper = document.createElement("mark");
-                    range.surroundContents(wrapper);
-                } else {
-                    console.log(range);
-                }
-            };
-
-            markerCommand.queryState = function() {
-                var selection = new scribe.api.Selection();
-                var range = selection.range;
-
-                return range.commonAncestorContainer.nodeName === this.nodeName
-                    || range.commonAncestorContainer.parentNode.nodeName === this.nodeName;
-            };
-
-            scribe.commands.marker = markerCommand;
-        };
-    }
-  });
+define("scribe-plugin-marker-command", ['scribe-common/src/element'], function($__0) {
+  "use strict";
+  var __moduleName = "scribe-plugin-marker-command";
+  if (!$__0 || !$__0.__esModule)
+    $__0 = {default: $__0};
+  var Element = $__0.default;
+  var $__default = function() {
+    return function(scribe) {
+      var getContainingMark = function(selection) {
+        return selection.getContaining(function(node) {
+          return node.nodeName === 'MARK';
+        });
+      };
+      var markerCommand = new scribe.api.Command('formatBlock');
+      markerCommand.nodeName = 'MARK';
+      markerCommand.execute = function() {
+        var selection = new scribe.api.Selection();
+        var range = selection.range;
+        selection.placeMarkers();
+        if (!this.queryState()) {
+          var wrapper = document.createElement('mark');
+          range.surroundContents(wrapper);
+        } else {
+          var markerNode = getContainingMark(selection);
+          Element.unwrap(markerNode.parentNode, markerNode);
+        }
+      };
+      markerCommand.queryState = function() {
+        var selection = new scribe.api.Selection();
+        return !!getContainingMark(selection);
+      };
+      scribe.commands.marker = markerCommand;
+    };
+  };
+  return {
+    get default() {
+      return $__default;
+    },
+    __esModule: true
+  };
+});
