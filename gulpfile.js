c936796bb46ddfa996e62bd5588db220f42f5a15
Ács Dániel <acsi@444.hu>
2014-08-28 12:33:38 +0200

    es6-module-transpiler -> traceur

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-var es6ModuleTranspiler = require('gulp-es6-module-transpiler');
+var traceur = require('gulp-traceur');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
     gulp.src('src/*.js')
-        .pipe(es6ModuleTranspiler({ type: 'amd' }))
+        .pipe(traceur({ modules: 'amd', moduleName: true }))
         .pipe(gulp.dest('build/'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
